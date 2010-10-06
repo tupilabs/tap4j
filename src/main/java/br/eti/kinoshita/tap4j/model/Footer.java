df2c18600ae5e1ad73379056abcbb4b58ba11592
kinow <kinow@4dc8f2a9-b67d-45e1-8787-ad175089815c>
2010-10-06 19:16:05 +0000

    Now the Footer is not a comment anymore. Instead of it it is a TAP plus some text

--- src/main/java/br/eti/kinoshita/tap4j/model/Footer.java
+++ src/main/java/br/eti/kinoshita/tap4j/model/Footer.java
@@ -23,16 +23,32 @@
  */
 package br.eti.kinoshita.tap4j.model;
 
+import java.io.Serializable;
+
 
 /**
+ * This class represents a Footer in the TAP Stream. A Footer, or tail is a mere 
+ * line with some text. In the very beginning there is a 'TAP' token and then 
+ * the text. For instance, 'TAP done' is a valid Footer.
+ * 
  * @author Bruno P. Kinoshita - http://www.kinoshita.eti.br
  * @since 1.0
  * @see {@link Comment}
  */
 public class Footer 
-extends Comment
+implements Serializable
 {
 
+	/**
+	 * Footer text.
+	 */
+	private String text;
+	
+	/**
+	 * Comment.
+	 */
+	private Comment comment;
+	
 	/**
 	 * Constructor that calls super class constructor.
 	 * 
@@ -41,7 +57,48 @@ extends Comment
 	 */
 	public Footer(String text)
 	{
-		super(text);
+		this.text = text;
+	}
+	
+	/**
+	 * @return Footer's text.
+	 */
+	public String getText()
+	{
+		return this.text;
+	}
+	
+	/**
+	 * @param comment Comment.
+	 */
+	public void setComment( Comment comment )
+	{
+		this.comment = comment;
+	}
+	
+	/**
+	 * @return Comment.
+	 */
+	public Comment getComment()
+	{
+		return this.comment;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append( "TAP " + this.text );
+		
+		if ( this.comment != null )
+		{
+			sb.append ( this.comment.toString() );
+		}
+		return "TAP " + this.text;
 	}
 
 }

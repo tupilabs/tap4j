e92bff8ee2e24355e66e432ddda91bcfa30d451a
cesar1983 <cesar1983@4dc8f2a9-b67d-45e1-8787-ad175089815c>
2011-03-18 17:08:37 +0000

    Test for TAP creation for Suites and Groups using listener SuiteTapReporter

---
+++ src/test/java/br/eti/kinoshita/tap4j/ext/testng/TestSuiteTapReporter.java
@@ -0,0 +1,40 @@
+package br.eti.kinoshita.tap4j.ext.testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author Cesar Fernandes de Almeida
+ *
+ * @since 18/03/2011
+ *
+ */
+@Listeners({ br.eti.kinoshita.tap4j.ext.testng.SuiteTAPReporter.class })
+public class TestSuiteTapReporter 
+{
+	@Test(groups="GropoTeste1")
+	public void teste1() 
+	{
+		Assert.assertNull(null);
+	}
+	
+	@Test(groups="GropoTeste1")
+	public void teste2() 
+	{
+		Assert.assertTrue(false);
+	}
+	
+	@Test(groups="GropoTeste2")
+	public void teste3() 
+	{
+		Assert.assertFalse(true);
+	}
+	
+	@Test(groups="GropoTeste2")
+	public void teste4() 
+	{
+		Assert.assertEquals("", "");
+	}
+}

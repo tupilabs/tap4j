fb39e691ea1f79bb9311ba6f1a5c5ae401bc50c9
Andy Campbell <ranford.b.crandal@gmail.com>
2014-08-22 19:06:46 -0400

    Fix issue when Yaml is contained in a diagnostic

---
+++ tap4j/src/test/java/org/tap4j/parser/issueGitHub33/TestYamlWithYamlInDiagnostics.java
@@ -0,0 +1,135 @@
+package org.tap4j.parser.issueGitHub33;
+
+import org.junit.Test;
+import org.tap4j.consumer.TapConsumer;
+import org.tap4j.consumer.TapConsumerFactory;
+import org.tap4j.model.TestResult;
+import org.tap4j.model.TestSet;
+import org.tap4j.parser.issue3406964.TestDirectives;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+/*
+*
+* If the diagnostics string that is produced contains more tap output with YAML in is diagnostics tap4j bails out, incorrectly deducing that the YAML in the diagnostics (which is at a different indentation level and should be ignored by the parser and just included as part of the diagnostic text) should be parsed and it falls over.
+
+
+The following tap stream demonstrates this issue, with the diagnostics actually containing a tap stream that is not part of the "main" tap stream.
+
+
+TAP version 13
+1..1
+ok 1 - sometest
+  ---
+    datetime: 2013-11-14T15:42:54
+    raw_output: |
+      Running sometest
+      .....
+      ================================================================================
+      Verification failed.
+
+          ---------------------
+          Framework Diagnostic:
+          ---------------------
+          ContainsSubstring failed.
+          --> The string must contain the substring.
+
+          Actual String:
+              TAP version 13
+              1..2
+                ---
+                  datetime: 2013-09-12T08:35:14
+                ...
+              ok 1 - testcases.SingleSilentTest
+                ---
+                  datetime: 2013-09-12T08:35:15
+                  raw_output:
+                ...
+              not ok 2 - testcases.SimpleTestWithSharedFixture
+                ---
+                  datetime: 2013-09-12T08:35:16
+                  raw_output:
+                ...
+
+          Expected Substring:
+
+              ok 2 - testcases.SimpleTestWithSharedFixture
+                ---
+                  datetime: 2013-09-12T08:35:16
+                ...
+
+      ================================================================================
+      Done sometest
+      __________
+  ...*/
+
+/**
+ * tap4j trips over YAML/TAP output that is included in the diagnostics of its own YAML
+ * @since 4.0.9
+ */
+public class TestYamlWithYamlInDiagnostics {
+
+    @Test
+    public void testYamlWithYamlInDiagnostics() {
+        TapConsumer tapConsumer = TapConsumerFactory.makeTap13YamlConsumer();
+        TestSet testSet = tapConsumer.load(new File(TestDirectives.class
+            .getResource("/org/tap4j/parser/issueGitHub33/issue-33_tap_stream.tap")
+            .getFile()));
+
+        assertEquals(1, testSet.getTestResults().size());
+        
+        TestResult tr1 = testSet.getTestResult(1);
+        Map<String, Object> yaml = tr1.getDiagnostic();
+        assertNotNull(yaml);
+
+        String multiLineYaml = (String) yaml.get("raw_output");
+
+        assertThat(multiLineYaml, is(equalTo(
+                                                     "Running sometest\n" +
+                                                     ".....\n" +
+                                                     "================================================================================\n" +
+                                                     "Verification failed.\n" +
+                                                     "\n" +
+                                                     "    ---------------------\n" +
+                                                     "    Framework Diagnostic:\n" +
+                                                     "    ---------------------\n" +
+                                                     "    ContainsSubstring failed.\n" +
+                                                     "    --> The string must contain the substring.\n" +
+                                                     "\n" +
+                                                     "    Actual String:\n" +
+                                                     "        TAP version 13\n" +
+                                                     "        1..2\n" +
+                                                     "          ---\n" +
+                                                     "            datetime: 2013-09-12T08:35:14\n" +
+                                                     "          ...\n" +
+                                                     "        ok 1 - testcases.SingleSilentTest\n" +
+                                                     "          ---\n" +
+                                                     "            datetime: 2013-09-12T08:35:15\n" +
+                                                     "            raw_output:\n" +
+                                                     "          ...\n" +
+                                                     "        not ok 2 - testcases.SimpleTestWithSharedFixture\n" +
+                                                     "          ---\n" +
+                                                     "            datetime: 2013-09-12T08:35:16\n" +
+                                                     "            raw_output:\n" +
+                                                     "          ...\n" +
+                                                     "\n" +
+                                                     "    Expected Substring:\n" +
+                                                     "\n" +
+                                                     "        ok 2 - testcases.SimpleTestWithSharedFixture\n" +
+                                                     "          ---\n" +
+                                                     "            datetime: 2013-09-12T08:35:16\n" +
+                                                     "          ...\n" +
+                                                     "\n" +
+                                                     "================================================================================\n" +
+                                                     "Done sometest\n" +
+                                                     "__________\n")));
+    }
+    
+}

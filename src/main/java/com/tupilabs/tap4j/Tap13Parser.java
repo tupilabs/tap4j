e3d2ac54a70f45c19ad928982f09817366e3c9a7
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-11-18 22:17:43 +1300

    Preprocess input, replacing strings. Handle test result comment

--- src/main/java/com/tupilabs/tap4j/Tap13Parser.java
+++ src/main/java/com/tupilabs/tap4j/Tap13Parser.java
@@ -30,7 +30,8 @@ import org.parboiled.common.Predicates;
 import org.parboiled.common.StringUtils;
 import org.parboiled.matchers.AnyOfMatcher;
 import org.parboiled.matchers.OneOrMoreMatcher;
-import org.parboiled.parserunners.TracingParseRunner;
+import org.parboiled.matchers.ZeroOrMoreMatcher;
+import org.parboiled.parserunners.BasicParseRunner;
 import org.parboiled.support.Characters;
 import org.parboiled.support.Filters;
 import org.parboiled.support.ParsingResult;
@@ -93,24 +94,23 @@ public class Tap13Parser extends BaseParser<Object> {
                         )
                 ),
                 Optional(
-                        // We need to use FirstOf here due to the Text rule of the Description consuming WS, which makes
-                        // it harder to have Description WS Directive (since the WS was consumed as part of Description)
+                        // Description
+                        Sequence(
+                                WS,
+                                Text().label("Description")
+                        )
+                ),
+                Optional(
                         FirstOf(
-                                // Description + Directive
-                                Sequence(
-                                        WS,
-                                        Text().label("Description"),
-                                        Directive()
-                                ),
-                                // Description
-                                Sequence(
-                                        WS,
-                                        Text().label("Description")
-                                ),
                                 // Directive
                                 Sequence(
                                         WS,
                                         Directive()
+                                ),
+                                // Comment
+                                Sequence(
+                                        WS,
+                                        Comment()
                                 )
                         )
                 ),
@@ -133,14 +133,14 @@ public class Tap13Parser extends BaseParser<Object> {
                 FirstOf("skip", "SKIP", "todo", "TODO").label("Directive Type"),
                 Optional(
                         WS,
-                        Text().label("Description")
+                        Text().label("Directive Description")
                 )
         );
     }
 
     Rule BailOut() {
         return Sequence(
-                String("Bail out!").label("Bail Out Constant"),
+                String("Bail out!").label("Bail Out Constant status"),
                 Optional(
                         Sequence(
                                 WS,
@@ -151,14 +151,30 @@ public class Tap13Parser extends BaseParser<Object> {
         ).label("Bail Out Line");
     }
 
-    Rule Diagnostics () {
+    Rule Diagnostics() {
         return Sequence(
-                "#",
+                String('#').label("Hash"),
                 Optional(
                         Text().label("Diagnostic")
                 ),
                 EOL
         ).label("Diagnostics Line");
+    }
+
+    Rule Comment() {
+        return Sequence(
+                String('#').label("Hash"),
+                Optional(
+                        OneOrMore(
+                                Sequence(
+                                        TestNot(AnyOf("\r\n")),
+                                        ANY
+                                )
+                        )
+                                .suppressSubnodes()
+                                .label("Comment Text")
+                )
+        ).label("Comment");
     }
 
     // --- Support rules
@@ -182,47 +198,68 @@ public class Tap13Parser extends BaseParser<Object> {
 
     public static final Rule EOL = new OneOrMoreMatcher(new AnyOfMatcher(Characters.of("\r\n"))).label("Newline");
 
-    public static final Rule WS = new OneOrMoreMatcher(new AnyOfMatcher(Characters.of(" \t\f"))).label("Whitespace");
+    public static final Rule WS = new ZeroOrMoreMatcher(new AnyOfMatcher(Characters.of(" \t\f"))).label("Whitespace");
 
     // --- Others
 
-    // we redefine the rule creation for string literals to automatically match trailing whitespace if the string
-    // literal ends with a space character, this way we don't have to insert extra whitespace() rules after each
-    // character or string literal
+    private String preprocess(String input) {
+        return input
+                // replace multiple spaces by single space
+                .replaceAll(" +", " ")
+                // replace trailing space
+                .replaceAll(" \n", "\n")
+                .replaceAll("(?m)^[ \t]*\r?\n", "");
+    }
 
-//    @Override
-//    protected Rule fromStringLiteral(String string) {
-//        return string.endsWith(" ") ?
-//                Sequence(String(string.substring(0, string.length() - 1)), WS) :
-//                String(string);
-//    }
+    public ParsingResult<Object> parse(String input) {
+        Tap13Parser parser = new Tap13Parser();
+        return new BasicParseRunner<>(parser.TestSet()).run(preprocess(input));
+    }
 
     // --- main method for testing
 
     public static void main(String[] args) {
         String input = "TAP Version 13\n" +
                 "1..20\n" +
-                "ok 1 the description is here\n" +
+                "ok 1 \n" +
+                "not ok 2 \n" +
+                "ok 3\n" +
                 "not ok\n" +
-                "not ok 13 # TODO bend space and time\n" +
-                "not ok 13 # todo bend space and time again\n" +
-                "ok 23 test # skip Insufficient amount pressure.\n" +
-                "not ok 13 # TODO bend space and time\n" +
-                "ok 23 # skip Insufficient amount pressure.\n" +
-                "Bail out! MySQL is not running.\n" +
-                "#\n" +
-                "# Create a new Board and Tile, then place\n" +
-                "# the Tile onto the board.\n" +
-                "#\n" +
+                "\n" +
+                "\n" +
+                "   \n" +
+                // "   ok 4" + // invalid!
+                "ok 5 this is a test\n" +
+                "not ok also this!\n" +
+                "ok 7 # TODO failed!\n" +
+                "ok 8 description with # SKIP ah\n" +
+                "not ok 9 # always fails\n" +
+                "ok 10 # always fails 2 \n" +
+                "ok #\n" +
+                "ok # \n" +
+                "ok  #  \n" +
+                "ok 14 Test # failed Class#method \n" +
+                // "not ok # a comment\n" +
+//                "not ok 13 # TODO bend space and time\n" +
+//                "not ok 13 # todo bend space and time again\n" +
+//                "ok 23 test # skip Insufficient amount pressure.\n" +
+//                "not ok 13 # TODO bend space and time\n" +
+//                "ok 23 # skip Insufficient amount pressure.\n" +
+//                "Bail out! MySQL is not running.\n" +
+//                "#\n" +
+//                "# Create a new Board and Tile, then place\n" +
+//                "# the Tile onto the board.\n" +
                 "";
         Tap13Parser parser = new Tap13Parser();
-        ParsingResult<Object> parsingResult = new TracingParseRunner<>(parser.TestSet()).run(input);
+        ParsingResult<Object> parsingResult = parser.parse(input);
         // ParsingResult<Object> parsingResult = new BasicParseRunner<>(parser.TestSet()).run(input);
         if (parsingResult.hasErrors()) {
             System.err.println("\n--- ParseErrors ---\n");
             System.err.println(StringUtils.join(parsingResult.parseErrors, "---\n"));
             System.err.println("\n--- ParseTree ---\n");
             System.err.println(printNodeTree(parsingResult, Filters.SKIP_EMPTY_OPTS_AND_ZOMS, Predicates.alwaysTrue()));
+        } else if (!parsingResult.matched) {
+            System.err.println("\n--- No Matches! ---\n");
         } else {
             System.out.println("\n--- ParseTree ---\n");
             String tree = printTree(
